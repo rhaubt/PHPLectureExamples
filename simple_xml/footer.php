d51ccc8d60aca67812e82f333fd407a43bb9017a
R Haubt <info@roberthaubt.com>
2021-04-12 15:50:53 +1000

    Add files via upload

---
+++ simple_xml/footer.php
@@ -0,0 +1,3 @@
+<html>
+	<p>This is a footer</p>
+</html>
---
+++ simple_xml/index.php
@@ -0,0 +1,6 @@
+<html>
+	<header><?php include ('header.php'); ?></header>
+	<h1>my first php file</h1>
+	<p><?php echo file_get_contents('example.txt');?></p>
+	<footer><?php include ('footer.php'); ?></footer>
+</html>
